bcefd232c90c56cfcd48eb1f4c316e305555c4f1
rckmnz <rckmnz05@gmail.com>
2018-03-13 17:36:40 -0400

    changed

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,7 @@ router.post("/register", (req, res) => {
             return res.redirect("/register");
         }
         passport.authenticate("local")(req, res, () => {
-            req.flash("success", "Welcome to YelpCamp " + user.username);
+            req.flash("success", "Welcome to Evergreen " + user.username);
             res.redirect("/campgrounds");
         });
     });
